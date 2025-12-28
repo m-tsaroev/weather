1a4566920964925b4f0feb1c52c8ca9d80e3d292
Muhammad <tsaroev17@gmail.com>
2025-12-28 18:37:36 +0300

    optimzed: WeatheraCard ui component requests

--- src/components/ui/WeatherCard/WeatherCard.tsx
+++ src/components/ui/WeatherCard/WeatherCard.tsx
@@ -1,13 +1,31 @@
 import { ForecastDay } from '@/components/ui/ForcastDay'
 import { ForcastNow } from '@/components/ui/ForcastNow'
-import { useGetForecastQuery } from '@/store/api/weatherApi.slice'
+import { useTypedSelector } from '@/hooks/useTypedSelector'
+import { useLazyGetForecastQuery } from '@/store/api/weatherApi.slice'
+import { useEffect, useState } from 'react'
 import styles from './WeatherCard.module.css'
 import type { WeatherCardProps } from './WeatherCard.types'
 
 const WeatherCard = (props: WeatherCardProps) => {
   const { city } = props
 
-  const { data, isLoading } = useGetForecastQuery({ city })
+  const [hasLoaded, setHasLoaded] = useState<boolean>(false)
+
+  const [getForecastQuery, { data, isLoading }] = useLazyGetForecastQuery()
+
+  const { activeCityName } = useTypedSelector((state) => state.cities)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await getForecastQuery({ city })
+
+      setHasLoaded(true)
+    }
+
+    if (activeCityName === city && !hasLoaded) {
+      fetchData()
+    }
+  }, [activeCityName, city, hasLoaded, getForecastQuery])
 
   return (
     <div className={styles.weatherCard}>
